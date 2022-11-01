6029f4a490c67f67d4e898fdc0eef311bddcba3a
Eben-Success <aebenezer237@gmail.com>
2022-11-01 13:04:51 +0000

    update

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Home page')
 
-def room(request):
-    return HttpResponse('ROOM')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
