41ce183563ac0f51b6127863b4fde9db3af14fb7
Eben-Success <aebenezer237@gmail.com>
2022-11-02 07:27:26 +0000

    sudo apt update

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -4,10 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class Room(models.Model):
-    #host = 
-    #topic = 
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # set topic to null when it's deleted and null=True, allow it to be deleted in the db.
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     # description can be empty, form can be empty while submitting form
     description = models.TextField(null=True, blank=True)
@@ -18,12 +25,6 @@ class Room(models.Model):
     # saves the first time we save an instance
     created = models.DateTimeField(auto_now_add=True)
 
-
-    def __str__(self):
-        return self.name
-
-class Topic(models.Model):
-    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
