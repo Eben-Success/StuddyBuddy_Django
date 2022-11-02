465d667543c86e3f670dbd91696e4142c3f5ec85
Eben-Success <aebenezer237@gmail.com>
2022-11-02 06:58:00 +0000

    update

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -24,4 +24,7 @@ class Room(models.Model):
 
 class Message(models.Model):
     #user = 
-    #room = 
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
