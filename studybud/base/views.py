c31a051e3ebde9ad8703ac7a57747615490123af
Eben-Success <aebenezer237@gmail.com>
2022-11-02 06:36:07 +0000

    update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -18,9 +18,6 @@ def home(request):
 
 # pass pk parameter in room
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room': room}
-    return render(request, 'base/room.html', context)
+    # id should be unique
+    room = Room.objects.get(id=pk)
+    return render(request, 'base/room.html')
