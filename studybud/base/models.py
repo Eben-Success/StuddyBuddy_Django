6d659f092f391ceabd834d9f4eb06b471e5b34e5
Eben-Success <aebenezer237@gmail.com>
2022-11-02 06:01:36 +0000

    update

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    #host = 
+    #topic = 
+    name = models.CharField(max_length=200)
+    # description can be empty, form can be empty while submitting form
+    description = models.TextField(null=True, blank=True)
