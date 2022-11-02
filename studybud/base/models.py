dedd96eff0ba98c2ff3c14759f5ae7578d5b583c
Eben-Success <aebenezer237@gmail.com>
2022-11-02 06:04:03 +0000

    update

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -8,3 +8,9 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     # description can be empty, form can be empty while submitting form
     description = models.TextField(null=True, blank=True)
+    #participants = 
+
+    # when saved, take a timestamp
+    updated = models.DateTimeField(auto_now=True)
+    # saves the first time we save an instance
+    created = models.DateTimeField(auto_now_add=True)
