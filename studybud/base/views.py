b4b5c87cbb032bb73886e31d2ecc88387c408d55
Eben-Success <aebenezer237@gmail.com>
2022-11-03 02:05:04 +0000

    update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -21,10 +21,12 @@ def home(request):
         )
     
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
     context = {
         'rooms': rooms,
-        'topics': topics
+        'topics': topics,
+        'room_count': room_count
     }
     return render(request, 'base/home.html', context)
 
