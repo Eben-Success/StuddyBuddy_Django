7d4218719cd3fd994f35dca27fe0375e24f2580a
Eben-Success <aebenezer237@gmail.com>
2022-11-07 13:40:20 +0000

    update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -89,6 +89,7 @@ def home(request):
 def room(request, pk):
     # id should be unique
     room = Room.objects.get(id=pk)
+    messages = room.message_set.all()
     return render(request, 'base/room.html')
 
 @login_required(login_url='login')
