c619dae4d2efc6d4f00be7028081c76121d3b548
Eben-Success <aebenezer237@gmail.com>
2022-11-17 15:59:28 +0000

    update

--- studybud/base/api/urls.py
+++ studybud/base/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoute),
     path('rooms/', views.getRooms),
+    path('rooms/<str:pk>/', views.getRoom),
 ]
--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
 
 
 @api_view(['GET'])
@@ -16,5 +17,14 @@ def getRoute(request):
 @api_view(['GET'])
 def getRooms(request):
     rooms = Room.objects.all()
-    return Response(rooms)
+    # many means serialize many objects
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
+    
     
